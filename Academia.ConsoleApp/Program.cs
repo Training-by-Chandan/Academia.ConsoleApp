329a97cde9f1dda5d2ce8d2fcc0c240ccc72e275
Chandan Bhagat <chandan.bhagat@outlook.com>
2020-09-21 11:36:18 +0545

    Add project files.

---
+++ Academia.ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Academia.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ans = "y";
+            do
+            {
+                Console.WriteLine("Enter your name");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter your class");
+                string classname = Console.ReadLine();
+
+                Student s1 = new Student(name, classname);
+
+                Console.WriteLine("\n\n\n");
+                s1.Display();
+
+                Console.WriteLine("Do you want to continue more (y/n)");
+                ans = Console.ReadLine();
+            } while (ans == "y");
+        }
+    }
+}
---
+++ Academia.ConsoleApp/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academia.ConsoleApp
+{
+    // nothing then public
+    // public, protected, private, internal, internal protected
+    //class Student 
+    //{
+
+
+    //}
+
+    public class Student
+    {
+        private string _name = "";
+        private string _class = "";
+        public Student()
+        {
+            _class = "6th Sem";
+        }
+
+        public Student(string name, string classname)
+        {
+            _name = name;
+            _class = classname;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Name = " + _name + "\nClass = " + _class);
+            Console.WriteLine(string.Format("Name = {0}\n Class = {1}", _name, _class));
+            Console.WriteLine($"Name = {_name}\nClass = {_class}");
+        }
+
+    }
+}
+
+
